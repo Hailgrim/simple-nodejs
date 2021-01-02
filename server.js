4266373f7736c7285786c244a2cb5f9685c5bc26
Hailgrim <mmorpglasg@gmail.com>
2021-01-02 19:54:24 +0300

    Work in progress

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ const fs = require('fs'); // модуль файловой системы
 const Readable = require('stream').Readable; // конструктор для создания читаемого потока, который берётся из модуля потоков
 const port = process.env.PORT || 5000;
 
-server.on('request', function(req, res){
+server.on('request', function (req, res) {
+	var links = req.url.split('/');
 
 	// Для всех GET-запросов будет отдаваться один и тот же каркас, который потом подгрузит нужную страницу
 	if (req.method == 'GET') {
-		var stream = new Readable();
-		var html = '<!DOCTYPE html><html><head><title>Simple NodeJS</title></head><body><h1>Hello World</h1></body></html>';
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html; charset=utf-8');
-		res.setHeader('Content-Length', Buffer.byteLength(html, 'utf8'));
-		stream.push(html);
-		stream.push(null); // указываем окончание вывода потока
-		stream.pipe(res);
+		if (links[1] == '') {
+			var stream = new Readable();
+			var html = '<!DOCTYPE html><html><head><title>Simple NodeJS</title></head><body><h1>Hello World</h1></body></html>';
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/html; charset=utf-8');
+			res.setHeader('Content-Length', Buffer.byteLength(html, 'utf8'));
+			stream.push(html);
+			stream.push(null); // указываем окончание вывода потока
+			stream.pipe(res);
+		}
 	}
-	
+
 });
 
 server.listen(port);
